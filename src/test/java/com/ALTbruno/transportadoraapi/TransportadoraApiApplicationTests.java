301fbdc531eb430ce866afc54ffc16c4c39a1b75
Bruno Barbosa <brb.dev@outlook.com>
2022-01-10 13:57:05 -0400

    inicio projeto

---
+++ src/test/java/com/ALTbruno/transportadoraapi/TransportadoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ALTbruno.transportadoraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportadoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
