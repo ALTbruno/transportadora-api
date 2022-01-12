55d5ed1042c9a2f9b8b76c0e22d1ae6f134d60b1
Bruno Barbosa <brb.dev@outlook.com>
2022-01-12 17:01:42 -0400

    jakarta persistance

--- src/main/java/com/ALTbruno/transportadoraapi/controller/ClienteController.java
+++ src/main/java/com/ALTbruno/transportadoraapi/controller/ClienteController.java
@@ -2,30 +2,21 @@ package com.ALTbruno.transportadoraapi.controller;
 
 import com.ALTbruno.transportadoraapi.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager entityManager;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Bruno");
-		cliente1.setEmail("email@mail.com");
-		cliente1.setTelefone("00000000000");
-
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Nome Cliente");
-		cliente2.setEmail("email@mail.com");
-		cliente2.setTelefone("00000000000");
-
-		return Arrays.asList(cliente1, cliente2);
+		return entityManager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 }
--- src/main/java/com/ALTbruno/transportadoraapi/model/Cliente.java
+++ src/main/java/com/ALTbruno/transportadoraapi/model/Cliente.java
@@ -1,12 +1,35 @@
 package com.ALTbruno.transportadoraapi.model;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String nome;
+
 	private String email;
+
+	@Column(name = "fone")
 	private String telefone;
 
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Cliente cliente = (Cliente) o;
+		return id.equals(cliente.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 
 	public Long getId() {
 		return id;
@@ -40,4 +63,3 @@ public class Cliente {
 		this.telefone = telefone;
 	}
 }
-
